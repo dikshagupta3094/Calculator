845b2fb2a7d37d2c57209780cec00310365bc838
Diksha Gupta <126059790+dikshagupta3094@users.noreply.github.com>
2023-06-19 09:51:00 +0530

    script.js

--- script.js
+++ script.js
@@ -3,18 +3,28 @@ let btn = document.querySelectorAll('.button');
 let input = document.getElementById("input")
 let string ="";
 
-//Making array using rest operator rest operator 
-let arr = [...btn]
 
-arr.forEach((btn)=>{
+[...btn].forEach((btn)=>{
     btn.addEventListener("click",(e)=>{
-    
-
+       
     if(e.target.innerHTML == "="){
-        string = eval(string)
-        input.value = string;
-    }
-
+        console.log(e)
+        try{
+            string = eval(string);
+            if(isNaN(string)){
+                throw new Error();
+            }
+            input.value = string
+        }
+        catch{
+             input.value = "Syntax error";
+               setTimeout(()=>{
+                string = ""
+                input.value= string
+                },2000)
+            }
+        }
+       
     else if(e.target.innerHTML == "AC"){
         string = ""
         input.value = string;
@@ -34,10 +44,11 @@ arr.forEach((btn)=>{
         string = string.substring(0,string.length-1);
         input.value = string;
     }
-
+    
     else{
-            string+= e.target.innerHTML
+            string+=e.target.innerHTML
             input.value = string;
     } 
     })
 })
+
